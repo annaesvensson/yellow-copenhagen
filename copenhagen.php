f16c97a9d76511e459f03ea7093f1c6bd043186a
markseu <mark2011@mayberg.se>
2025-09-15 20:33:21 +0200

    Renamed Markdown block elements

--- copenhagen.php
+++ copenhagen.php
@@ -2,7 +2,7 @@
 // Copenhagen extension, https://github.com/annaesvensson/yellow-copenhagen
 
 class YellowCopenhagen {
-    const VERSION = "0.9.2";
+    const VERSION = "0.9.3";
     public $yellow;         // access to API
     
     // Handle initialisation
